e52f39691c854a22ce9a274d3e6f8c3564900b14
Vlado <petrovic.vlado96@gmail.com>
2024-12-30 22:56:31 +0100

    sql coding

--- Market_db.session.sql
+++ Market_db.session.sql
@@ -8,6 +8,27 @@ SELECT * FROM item WHERE owner IS NOT NULL;
 
 SELECT * FROM item;
 
+SELECT count(*) FROM item;
+
+SELECT avg(price) FROM item;
+
+SELECT sum(price) FROM item;
+
+/* Inner Join  */
+SELECT item.name, item.price , user.userName
+FROM item
+INNER JOIN user ON item.owner = user.id;
+
+/* Left Join */
+SELECT user.userName, item.name, item.owner
+FROM user
+LEFT JOIN item ON user.id = item.owner
+ORDER BY user.userName;
+
+
+
+
+
 /* USER SQL Query  */
 
 
@@ -19,3 +40,8 @@ SELECT userName, email_address FROM user ORDER BY userName ASC;
 
 SELECT userName, email_address, budget FROM user WHERE budget<600 AND userName LIKE 'J%'; 
 
+SELECT user.userName, count(item.owner) as item_count
+FROM item
+INNER JOIN user ON user.id = item.owner
+GROUP BY userName
+HAVING Count(item.owner) > 1;
